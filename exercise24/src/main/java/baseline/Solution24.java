72f33b77ba1eef2d72caae91e43d60d84dc360ad
Leonardo Herrera <leo2399@gmail.com>
2021-09-25 02:54:08 -0400

    first commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,18 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution24 {
+    private static final Scanner  in = new Scanner(System.in);
+
+    static boolean isAnagram(String Str1, String Str2){
+        // if the lengths the strings don't match, return false
+        // check both strings for same characters
+        // return true if they are equal
+    }
+
+    public static void main(String[] args) {
+        // prompt user for both strings
+        // call isAnagram function
+    }
+}
