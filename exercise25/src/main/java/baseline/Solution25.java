08d4466dc66ba91c2081b09e4a41f6879fb38e4f
Leonardo Herrera <leo2399@gmail.com>
2021-09-29 21:15:27 -0400

    first commit

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Leonardo Herrera
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution25 {
+
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void passwordValidator(String Str){
+        //Str is only numbers and Str.length < 8
+        //"password is very weak"
+        // add 25
+
+        //Str is only letters and Str.length < 8
+        //"password is weak"
+        // add 50
+
+        //Str contains letter snd at least one number && Str.length >= 8
+        //"password is strong"
+        // add 75
+
+        //Str contains letters, numbers, and special characters, and Str.length >= 8
+        //"password is very strong"
+        // add 100
+
+        //Str contains none of the above
+        //"password is of unknown strength"
+        // 0
+
+        //display password strength as numeric value
+    }
+
+    public static void main(String[] args) {
+        //"Enter password"
+        //call passwordValidator
+
+    }
+}
+
