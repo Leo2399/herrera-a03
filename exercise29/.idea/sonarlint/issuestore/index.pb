f41e17bbf4810098093f945da91a9b5f15d63488
Leonardo Herrera <leo2399@gmail.com>
2021-10-02 02:49:06 -0400

    initial commit

---
+++ exercise29/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution29.java,9\5\95454940577f71189b8edc339b7b513e0a91fe12
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution29 {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int r;
+        int years;
+
+        //ask for rate of return
+
+        //if r is 0 or if r is not a number
+        //r is invalid, ask for rate of return again
+
+        //print the amount of years to double your investment
+
+    }
+}
