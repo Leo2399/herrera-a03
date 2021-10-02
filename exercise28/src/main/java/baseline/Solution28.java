e5f44aa9b3614e700078b1a9e2cc27267ada12f0
Leonardo Herrera <leo2399@gmail.com>
2021-10-02 02:33:05 -0400

    intitial commit

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,18 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28 {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num;
+        int total;
+
+        //for (int i = 1; i<=5; i++)
+
+        //ask user to enter number
+
+        //add up all numbers for total
+    }
+}
