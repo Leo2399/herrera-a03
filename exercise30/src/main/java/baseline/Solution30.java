1e8213c6f7f97c7cdfd2b0ba581887fb1a425142
Leonardo Herrera <leo2399@gmail.com>
2021-10-02 03:27:53 -0400

    initial commit

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+        //for loop for the columns
+            //for loop for the rows
+                //print the table
+    }
+}
