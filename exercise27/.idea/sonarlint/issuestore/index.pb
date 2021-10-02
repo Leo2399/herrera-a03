79e2930b3093877a54fedcd2c1d2f633af63d1e1
Leonardo Herrera <leo2399@gmail.com>
2021-10-02 02:21:10 -0400

    initial commit

---
+++ exercise27/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+V
+&src/main/java/baseline/Solution27.java,c\4\c4ea424aa0247fa241423fcc7495bda62cff644e
---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,62 @@
+package baseline;
+import java.util.Scanner;
+
+public class Solution27 {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //prompt user for first name
+        //prompt user for last name
+        //prompt user for id
+        //prompt user for zip code
+        //validate inputs
+    }
+
+    public boolean inputFirstName(String first){
+        //if first name < 2
+        //first name is invalid
+
+        //else if first name is empty
+        //first name is invalid
+
+        //otherwise, first name is valid
+
+    }
+
+    public boolean inputLastName(String last){
+        //if last name < 2
+        //last name is invalid
+
+        //else if last name is empty
+        //last name is invalid
+
+        //otherwise, last name is valid
+
+    }
+
+    public boolean inputID(String Id){
+        //if the ID is not in AA-1234 format
+        //Id is invalid
+
+        //otherwise, Id is valid
+    }
+
+    public boolean inputZipCode(int zip){
+        //if zip is not a number
+        //zip is invalid
+
+        //if zip < 5
+        //zip is invalid
+
+        //otherwise, zip is valid
+    }
+
+    public void validateInput(String first, String last, String id, int zip){
+
+        //check to make sure that all the validation rules are true
+
+        //print "no errors" if they are all valid
+
+    }
+}
