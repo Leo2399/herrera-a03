efb684dca319916314c5292b9dddceecdfbbb4a3
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 03:38:05 -0400

    second commit

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -7,15 +7,29 @@ public class Solution29 {
     private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int r;
         int years;
+        boolean status = false;
+        while(true){
+            try{
+                //ask for rate of return
+                System.out.print("What is the rate of return? ");
+                String r = in.nextLine();
+                
+                years = 72/Integer.parseInt(r);
 
-        //ask for rate of return
+                //print the amount of years to double your investment
+                System.out.println("It will take "+years+ " years to double your initial investment.");
+                status = true;
+            }
 
-        //if r is 0 or if r is not a number
-        //r is invalid, ask for rate of return again
-
-        //print the amount of years to double your investment
-
+            //if r is 0 or if r is not a number
+            //r is invalid, ask for rate of return again
+            catch (NumberFormatException | ArithmeticException e){
+                System.out.println("Sorry. That's not a valid input.");
+            }
+            if(status){
+                break;
+            }
+        }
     }
 }
