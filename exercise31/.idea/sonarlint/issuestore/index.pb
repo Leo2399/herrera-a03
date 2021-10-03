4ef66a7a441f29cc171f1523e6329abee32c33c7
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 04:11:35 -0400

    initial commit

---
+++ exercise31/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution31.java,5\c\5cafb99e82926136c2f1d044c79185ad9c24e74e
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution31 {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //use try and catch loop to make sure you can't continue without a number
+
+        //ask for age
+
+        //ask for heart rate
+
+        //print age and heart rate
+
+        //set up table headers
+
+        //call heartRate(age, heartrate, 55)
+
+    }
+
+    public void heartRate(int age, int restHR, int intensity){
+
+        //heart rate <= 95
+        //targetHR = (((220 - age) - restingHR) * intensity) + restingHR
+        //intensity incremented by 5
+    }
+
+    }
+
