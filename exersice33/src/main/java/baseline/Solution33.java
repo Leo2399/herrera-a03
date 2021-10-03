d83b54432fb51e41a62a18c6e2cf29be620cb8f4
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 19:22:43 -0400

    initial commit

---
+++ exersice33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,18 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution33 {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //store the strings in an array
+        //make a random number generator equal to the number of strings in the array
+
+        //ask the question
+
+        //display random answer
+
+    }
+}
