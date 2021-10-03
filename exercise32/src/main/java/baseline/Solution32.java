6576b4c7eaf09c8e49c60a90e7db6655dacb6b67
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 04:35:31 -0400

    intitial commit

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,42 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution32 {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //"Let's play the Guess the number game!"
+
+        //ask user for difficulty
+
+        //if choice is 1, guess from 1-10
+
+        //if choice is 2, guess from 1-100
+
+        //if choice is 3, guess from 1-1000
+
+        //ask if you want to play again
+    }
+
+    public void easy(){
+        //guess from 1-10
+        //if too low guess again
+        //if too high guess again
+        //if guessed right return the number of guesses
+    }
+
+    public void medium(){
+        //guess from 1-100
+        //if too low guess again
+        //if too high guess again
+        //if guessed right return number of guesses
+    }
+
+    public void hard(){
+        //guess from 1-1000
+        //if too low guess again
+        //if too high guess again
+        //if guesses right return number of guesses
+    }
+}
