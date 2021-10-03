482df4f0115259861efaa902f74834b0bc17abd4
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 19:01:19 -0400

    second commt

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -4,39 +4,132 @@ import java.util.Scanner;
 
 public class Solution32 {
     private static Scanner in = new Scanner(System.in);
+    public static String tooLow = "Too low. Guess again: ";
+    public static String tooHigh = "Too high. Guess again: ";
 
     public static void main(String[] args) {
+
+        String playAgain = "Y";
+
         //"Let's play the Guess the number game!"
+        System.out.println("Let's play the Guess the number game!");
+        while(playAgain.matches("^[Yy].*")) {
+            //ask user for difficulty
+            System.out.print("Enter the difficulty level (1, 2, or 3): ");
+            int choice = in.nextInt();
 
-        //ask user for difficulty
+            switch (choice) {
 
-        //if choice is 1, guess from 1-10
+                //if choice is 1, guess from 1-10
+                case 1:
+                    easy();
+                    break;
 
-        //if choice is 2, guess from 1-100
+                //if choice is 2, guess from 1-100
+                case 2:
+                    medium();
+                    break;
 
-        //if choice is 3, guess from 1-1000
+                //if choice is 3, guess from 1-1000
+                case 3:
+                    hard();
+                    break;
+            }
 
-        //ask if you want to play again
+            //ask if you want to play again
+            System.out.print("Do you wish to play again?(Y/N) ");
+            playAgain = in.next();
+
+            if(playAgain.matches("^[Nn].*")){
+                break;
+            }
+        }
     }
 
-    public void easy(){
+    public static void easy(){
         //guess from 1-10
-        //if too low guess again
-        //if too high guess again
-        //if guessed right return the number of guesses
+        int randNum = (int) (Math.random()*10 + 1);
+        int guess=0;
+        int totalGuesses=0;
+
+        System.out.print("I have my number. What's your guess? ");
+
+        while (guess!=randNum){
+            guess = in.nextInt();
+            totalGuesses++;
+
+            //if too low guess again
+            if(guess<randNum){
+                System.out.print(tooLow);
+            }
+
+            //if too high guess again
+            else if(guess>randNum){
+                System.out.print(tooHigh);
+            }
+
+            //if guessed right return number of guesses
+            else if(guess==randNum){
+                System.out.println("Congrats! You got it in "+totalGuesses+" guesses.");
+            }
+        }
     }
 
-    public void medium(){
+    public static void medium(){
+
         //guess from 1-100
-        //if too low guess again
-        //if too high guess again
-        //if guessed right return number of guesses
+        int randNum = (int) (Math.random()*100 + 1);
+        int guess = 0;
+        int totalGuesses = 0;
+
+        System.out.print("I have my number. What's guess: ");
+        while(guess!=randNum){
+            guess = in.nextInt();
+            totalGuesses++;
+
+            //if too low guess again
+            if(guess<randNum){
+                System.out.print(tooLow);
+            }
+
+            //if too high guess again
+            else if(guess>randNum){
+                System.out.print(tooHigh);
+            }
+
+            //if guessed right return number of guesses
+            else if(guess==randNum){
+                System.out.println("Congrats! You got in "+totalGuesses+" guesses.");
+            }
+        }
     }
 
-    public void hard(){
+    public static void hard(){
+        int randNum = (int) (Math.random()*1000 + 1);
+        int guess = 0;
+        int totalGuesses = 0;
+
         //guess from 1-1000
-        //if too low guess again
-        //if too high guess again
-        //if guesses right return number of guesses
+        System.out.print("I have my number. What's your guess: ");
+
+        while(guess!=randNum){
+            guess = in.nextInt();
+            totalGuesses++;
+
+            //if too low guess again
+            if(guess<randNum){
+                System.out.print(tooLow);
+            }
+
+            //if too high guess again
+            else if(guess>randNum){
+                System.out.print(tooHigh);
+            }
+
+            //if guessed right return number of guesses
+            else if(guess==randNum){
+                System.out.println("Congrats! You got it in "+totalGuesses+" guesses.");
+            }
+        }
     }
 }
