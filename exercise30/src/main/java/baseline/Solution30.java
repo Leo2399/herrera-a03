363d49022afeb19dccd93532e0e0f48f38ca9bd0
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 03:40:18 -0400

    second commit

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -4,7 +4,13 @@ public class Solution30 {
 
     public static void main(String[] args) {
         //for loop for the columns
+        for(int i=1; i<=12; i++){
             //for loop for the rows
+            for(int j=1; j<=12; j++){
                 //print the table
+                System.out.print((i*j+"\t"));
+            }
+            System.out.println();
+        }
     }
 }
