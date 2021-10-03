53e90d91b876ea06862e41314ccecc62b53531ac
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 02:23:45 -0400

    second commit

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -1,21 +1,34 @@
 package baseline;
 
 public class PaymentCalculator {
-   private int months;
-   private int apr;
-   private int balance;
-   private int payments;
-   public int dailyRate = apr/365;
 
- public void setCalculate(int b, int p, int apr){
+   public double months = 0;
+   private double balance;
+   private double payments;
+   private double dailyRate;
+
+ public void setBalance(double b){
      // set balance
-     // set payments
-     // set apr
+     this.balance = b;
+ }
+
+ public void setPayments(double p){
+     // set monthly payments
+     this.payments = p;
+ }
+
+ public void setDailyRate(double apr){
+     // set daily rate
+     this.dailyRate = (apr/100)/365;
  }
 
  public int calculateMonthsUntilPaidOff(){
-     // months = -(1/30) * log(1 + balance/payments * (1 - (1 + dailyRate)^30)) / log(1 + dailyRate)
+
+     //months = -(1/30) * log(1 + balance/payments * (1 - (1 + dailyRate)^30)) / log(1 + dailyRate)
+     months = Math.ceil(-(1/30.0) * Math.log(1 + balance/payments * (1 - Math.pow(1 + dailyRate, 30))) / Math.log(1 + dailyRate));
+
      // return months
+     return (int) months;
  }
 
 }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -10,14 +10,23 @@ public class Solution26 {
         PaymentCalculator pay = new PaymentCalculator();
 
         // prompt user for the balance
+        System.out.print("What is your balance? ");
+        double balance = in.nextDouble();
+        pay.setBalance(balance);
 
         // prompt user for APR as a percent
         // dailyRate = APR/365
+        System.out.print("What is the APR on the card (as a percent)? ");
+        double apr = in.nextDouble();
+        pay.setDailyRate(apr);
 
         // prompt user for monthly payments
+        System.out.print("What is the monthly payment you can make? ");
+        double payments = in.nextDouble();
+        pay.setPayments(payments);
 
         // calculate months until paid off
-
         // print the number of months left until card is paid off
+        System.out.println("It will take you "+pay.calculateMonthsUntilPaidOff()+" months to pay off this card");
     }
 }
