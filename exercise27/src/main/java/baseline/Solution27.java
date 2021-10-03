fb4d9909c7a3af37cc87cd2ef53dc3d8a06110e7
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 02:28:20 -0400

    second commit

--- exercise27/src/main/java/baseline/Solution27.java
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -1,3 +1,8 @@
+/*
+*  UCF COP3330 Fall 2021 Assignment 3 Solutions
+*  Copyright 2021 Leonardo Herrera
+*/
+
 package baseline;
 import java.util.Scanner;
 
@@ -7,56 +12,112 @@ public class Solution27 {
 
     public static void main(String[] args) {
         //prompt user for first name
+        System.out.print("Enter the first name: ");
+        String firstName = in.nextLine();
+
         //prompt user for last name
-        //prompt user for id
+        System.out.print("Enter the last name: ");
+        String lastName = in.nextLine();
+
         //prompt user for zip code
+        System.out.print("Enter the ZIP code: ");
+        String zip = in.nextLine();
+
+        //prompt user for id code
+        System.out.print("Enter the employee ID: ");
+        String id = in.nextLine();
+
         //validate inputs
+        validateInput(firstName, lastName,  id, zip);
     }
 
-    public boolean inputFirstName(String first){
+    public static boolean inputFirstName(String first){
+
+        boolean status = true;
+
         //if first name < 2
         //first name is invalid
+        if(first.length()<2){
+            System.out.println("The first name must be at least 2 characters long.");
+            status = false;
+        }
 
         //else if first name is empty
         //first name is invalid
+        if(first.length()==0){
+            System.out.println("The first name must be filled in.");
+            status = false;
+        }
 
         //otherwise, first name is valid
-
+        return status;
     }
 
-    public boolean inputLastName(String last){
+    public static boolean inputLastName(String last){
+
+        boolean status = true;
         //if last name < 2
         //last name is invalid
+        if(last.length()<2){
+            System.out.println("The last name must be at least 2 characters long.");
+            status = false;
+        }
 
         //else if last name is empty
         //last name is invalid
+        if(last.length()==0){
+            System.out.println("The last name must be filled in.");
+            status = false;
+        }
 
         //otherwise, last name is valid
+        return status;
 
     }
 
-    public boolean inputID(String Id){
-        //if the ID is not in AA-1234 format
-        //Id is invalid
+    public static boolean inputID(String Id){
+
+        //if the ID is in AA-1234 format
+        //Id is valid
+        if(Id.matches("^[a-zA-Z]{2}[-][0-9]{4}.*")){
+            return true;
+        }
 
         //otherwise, Id is valid
+        else{
+            System.out.println("The employee must be in the format of AA-1234");
+            return false;
+        }
     }
 
-    public boolean inputZipCode(int zip){
-        //if zip is not a number
-        //zip is invalid
-
-        //if zip < 5
-        //zip is invalid
+    public static boolean inputZipCode(String zip){
+        //if zip is a number and is a 5-digit number
+        //zip is valid
+        if(zip.matches("^[0-9].*") && zip.length()==5){
+            return true;
+        }
 
         //otherwise, zip is valid
+        else{
+            System.out.println("The zipcode must be a 5 digit number");
+            return false;
+        }
     }
 
-    public void validateInput(String first, String last, String id, int zip){
+    public static boolean validateInput(String first, String last, String id, String zip){
+        boolean status = false;
 
         //check to make sure that all the validation rules are true
+        boolean firstname = inputFirstName(first);
+        boolean lastname = inputLastName(last);
+        boolean empId = inputID(id);
+        boolean zipcheck = inputZipCode(zip);
 
-        //print "no errors" if they are all valid
-
+        if(firstname && lastname && empId && zipcheck){
+            //print "no errors" if they are all valid
+            System.out.println("There were no errors found.");
+            status = true;
+        }
+        return status;
     }
 }
