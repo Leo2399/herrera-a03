eab4e0540fbfa15ccf68b7de49dc18e4a59f9f9d
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 20:22:50 -0400

    initial commit

---
+++ exercise34/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution34.java,0\5\05674b502c60f30dfbe8b315f70576e1f49e3470
---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution34 {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //store 5 names in an array list
+
+        //ask which name to remove
+
+        //removeName
+
+        //display list again with remaining names
+    }
+
+    public static void removeName(ArrayList names, String delName){
+
+    }
+}
