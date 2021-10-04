dd62160b388b56cec6948ec76e714fe07b5b659c
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 22:46:16 -0400

    second commit

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -1,16 +1,30 @@
 package baseline;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Solution35 {
     private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String name;
+        ArrayList<String> storedName = new ArrayList<>();
+        Random winner = new Random();
+
         //while string input is not empty
-        //input name and store into an array list
+        do{
+            System.out.print("Enter a name: ");
+            name = in.nextLine();
 
-        //pick random name as the winner
-        //display winner
+            //input name and store into an array list
+            storedName.add(name);
+
+            //pick random name as the winner
+            //display winner
+            if(name.length()==0){
+                System.out.println("The winner is..... "+storedName.get(winner.nextInt(storedName.size())));
+            }
+        }while(name.length()!=0);
     }
-
 }
