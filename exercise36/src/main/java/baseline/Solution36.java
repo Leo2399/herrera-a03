5d7403e0364380459efb49d6275eeb50395f84cb
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 21:28:23 -0400

    initial commit

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,41 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution36 {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //ask user to enter a number
+        //store names in an array
+
+        //if user inputs "done"
+        //get the average
+        //get max
+        //get min
+        //get Std
+
+    }
+
+    public int average(ArrayList numbers){
+        //average = sumofnumbers/totalnumbers
+    }
+
+    public int max(ArrayList numbers){
+
+        //find the largest number in the list of numbers
+
+    }
+
+    public int min(ArrayList numbers){
+
+        //find the smallest numbers in the list of numbers
+    }
+
+    public int Std(ArrayList numbers){
+        //std = sqrt(sumof(numbers - mean)^2 / total numbers
+    }
+}
