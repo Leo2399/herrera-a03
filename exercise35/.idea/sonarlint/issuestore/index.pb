80578243840f4812160be89700f7f8f8b714b695
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 20:50:21 -0400

    initial commit

---
+++ exercise35/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+V
+&src/main/java/baseline/Solution35.java,b\6\b6ee7cc313bae0851e64e978e1ca42ae2041cd61
---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,16 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution35 {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //while string input is not empty
+        //input name and store into an array list
+
+        //pick random name as the winner
+        //display winner
+    }
+
+}
