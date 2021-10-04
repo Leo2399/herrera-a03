847a2d1b692a3d88159a8d88fd30e8fb5b6f6d54
Leonardo Herrera <leo2399@gmail.com>
2021-10-03 22:00:03 -0400

    second commit

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -8,16 +8,25 @@ public class Solution34 {
     private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        ArrayList<String> employee = new ArrayList();
+        System.out.println("There are 5 employees");
         //store 5 names in an array list
+        employee.add("John Smith");
+        employee.add("Jackie Johnson");
+        employee.add("Chris Johnson");
+        employee.add("Amanda Cullen");
+        employee.add("Jeremy Goodwin");
+        System.out.println(employee);
 
         //ask which name to remove
+        System.out.print("Enter an employee name to remove: ");
+        String delete = in.nextLine();
 
         //removeName
+        employee.remove(delete);
 
         //display list again with remaining names
-    }
-
-    public static void removeName(ArrayList names, String delName){
-
+        System.out.println("There are 4 employees");
+        System.out.println(employee);
     }
 }
